e5af22d22d7c7ca8e6cc99fec7525fdba8a7f0e3
Minijump <sacha.jumpertz@gmail.com>
2026-01-25 08:21:58 +0100

    Add vote function

--- MiniDao/DaoIP.sol
+++ MiniDao/DaoIP.sol
@@ -1,19 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+error AlreadyVoted();
+
 
 contract DaoIP {
     // TODOs
-    //  vote
     //  get_votes
     //  closeIP (only owners)
     string name;
     string description;
     address public immutable i_proposer;
 
+    enum Vote { Abstain, Approve, Reject }
+    mapping(address => Vote) public votes;
+    uint256 votesCount = 0;
+    uint256 approveCount = 0;
+    address[] public voters;
+
     constructor(string memory _name, string memory _description) {
         name = _name;
         description = _description;
         i_proposer = msg.sender;
+    }
+
+    function vote(Vote _vote) external {
+        if (votes[msg.sender] != Vote.Abstain){
+            revert AlreadyVoted();
+        }
+        voters.push(msg.sender);
+        votes[msg.sender] = _vote;
+        if (_vote == Vote.Reject || _vote == Vote.Approve) {
+            votesCount += 1;
+        }
+        if (_vote == Vote.Approve) {
+            approveCount += 1;
+        }
     }
 }
