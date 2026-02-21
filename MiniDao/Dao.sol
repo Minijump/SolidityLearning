fb377925ce05c02bf7b0fe2e529529e392c5c521
Minijump <sacha.jumpertz@gmail.com>
2026-02-21 14:01:25 +0100

    Imp and simplify Doa

--- MiniDao/Dao.sol
+++ MiniDao/Dao.sol
@@ -5,28 +5,33 @@ import {DaoIP} from "./DaoIP.sol";
 
 
 contract Dao {
-    struct Daoproposal{
-        address proposalAddress;
-        DaoIP proposal;
-    }
-
-    mapping(address => Daoproposal) public proposals;
+    mapping(address => DaoIP) public proposals;
     address[] public proposalList;
 
     function createProposal(string memory _name, string memory _description) public{
         DaoIP newProposal = new DaoIP(_name, _description);
         address proposalAddress = address(newProposal);
-        proposals[proposalAddress] = Daoproposal(proposalAddress, newProposal);
+        proposals[proposalAddress] = newProposal;
         proposalList.push(proposalAddress);
     }
 
     function approveProposal(address _proposalAddress) public{
-        DaoIP proposal = proposals[_proposalAddress].proposal;
+        DaoIP proposal = proposals[_proposalAddress];
         proposal.vote(DaoIP.Vote.Approve);
     }
 
     function rejectProposal(address _proposalAddress) public{
-        DaoIP proposal = proposals[_proposalAddress].proposal;
+        DaoIP proposal = proposals[_proposalAddress];
         proposal.vote(DaoIP.Vote.Reject);
+    }
+
+    function closeProposal(address _proposalAddress) public{
+        DaoIP proposal = proposals[_proposalAddress];
+        proposal.closeIP();
+    }
+
+    function getProposalResult(address _proposalAddress) public view returns (uint256, uint256){
+        DaoIP proposal = proposals[_proposalAddress];
+        return (proposal.getVotesResults());
     }
 }
