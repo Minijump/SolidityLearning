9b66350a72cc91b94b61dfb13c231e9280ab10fc
SJ <sacha.jumpertz@gmail.com>
2026-02-22 08:04:22 +0100

    fix forge compile notes

--- FoundrySimpleStorage/src/SimpleStorage.sol
+++ FoundrySimpleStorage/src/SimpleStorage.sol
@@ -3,19 +3,19 @@ pragma solidity 0.8.18;
 
 contract SimpleSorage {
     struct Person {
-        uint256 my_favorite_number;
+        uint256 myFavouriteNumber;
         string name;
     }
 
     bool hasFavoriteNumber = true;
     uint256 public favoriteNumber = 88;
-    uint256[] list_of_favorite_numbers = [1, 2, 3, 4, 5];
+    uint256[] listOfFavouriteNumbers = [1, 2, 3, 4, 5];
     string favoriteNumberInText = "eighty-eight";
     int256 favoriteInt = -88;
     address myAddress = 0xCc78E8a3515072277A46f940E5762D7e00669e47;
     bytes32 favoriteBytes32 = "cat";
-    Person public my_friend = Person(7, 'Pat');
-    Person[] public list_of_people;
+    Person public myFriend = Person({ myFavouriteNumber: 7, name: 'Pat' });
+    Person[] public listOfPeople;
     mapping (string => uint256) public nameToFavoriteNumber;
 
 
@@ -23,16 +23,16 @@ contract SimpleSorage {
         favoriteNumber = _favoriteNumber;
     }
 
-    function retrieve_var() public view returns(uint256){
+    function retrieveVar() public view returns(uint256){
         return favoriteNumber;
     }
 
-    function retrieve_cst() public pure returns(uint256){
+    function retreiveCst() public pure returns(uint256){
         return 7;
     }
 
-    function add_person(string memory _name, uint256 _favorite_number) public {
-        list_of_people.push(Person(_favorite_number, _name));
-        nameToFavoriteNumber[_name] = _favorite_number;
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person({ myFavouriteNumber: _favoriteNumber, name: _name }));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
