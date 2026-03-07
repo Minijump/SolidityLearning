fbc3b83b86b99419b90f564b0fb61e91e4ae0dd6
SJ <sacha.jumpertz@gmail.com>
2026-03-07 13:50:33 +0100

    continue test and add script

--- FoundryFundMe/test/FundMeTest.t.sol
+++ FoundryFundMe/test/FundMeTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
@@ -14,5 +14,14 @@ contract FundMeTest is Test {
     function testMinIsFive() public view{
         uint256 min = fundMe.MINIMUM_USD();
         assertEq(min, 5 * 1e18);
+    }
+
+    function testOwnerIsMsgSender() public view{
+        address owner = fundMe.iOwner();
+        console.log("owner: ", owner);
+        console.log("msg.sender: ", msg.sender);
+        console.log("address(this): ", address(this));
+        // We deploy test contract, the test contract deploy the FundMe contract, so the owner of the FundMe contract is the test contract
+        assertEq(owner, address(this));
     }
 }
