b9f90f587c1dd672819d0d2ba2c01d57a58377f4
SJ <sacha.jumpertz@gmail.com>
2026-03-07 15:20:07 +0100

    factorize UT

--- FoundryFundMe/test/FundMeTest.t.sol
+++ FoundryFundMe/test/FundMeTest.t.sol
@@ -9,39 +9,44 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
     address USER = makeAddr("user");
+    uint256 constant FUND_AMOUNT = 1e18;
 
     function setUp() external {
         DeployFundMe deployer = new DeployFundMe();
         fundMe = deployer.run();
+        vm.deal(USER, 5e18);
     }
 
     function testMinIsFive() public view{
         uint256 min = fundMe.MINIMUM_USD();
+
         assertEq(min, 5 * 1e18);
     }
 
     function testOwnerIsMsgSender() public view{
         address owner = fundMe.iOwner();
+
         assertEq(owner, msg.sender);
     }
 
     function testFundFailsWithoutEnoughEth() public {
         vm.expectRevert();
+
         fundMe.fund{value: 0}();
     }
 
     function testFundUpdatesFundedDataStructure() public {
-        vm.deal(USER, 1e18);
         vm.prank(USER);
-        fundMe.fund{value: 1e18}();
+        fundMe.fund{value: FUND_AMOUNT}();
+
         uint256 amountFunded = fundMe.addressToAmountFunded(USER);
-        assertEq(amountFunded, 1e18);
+        assertEq(amountFunded, FUND_AMOUNT);
     }
 
     function testAddsFunderToArrayOfFunders() public {
-        vm.deal(USER, 1e18);
         vm.prank(USER);
-        fundMe.fund{value: 1e18}();
+        fundMe.fund{value: FUND_AMOUNT}();
+
         address funder = fundMe.funders(0);
         assertEq(funder, USER);
     }
