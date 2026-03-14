0a2d7d9d51d6ca5c39589a1980f5336552eb3732
SJ <sacha.jumpertz@gmail.com>
2026-03-14 13:31:40 +0100

    add enter raffle logic

--- SimpleLottery/src/Raffle.sol
+++ SimpleLottery/src/Raffle.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract Raffle {}
+error Raffle_NotEnoughETHEntered();
+
+/**
+ * @title raffle contract
+ * @author me
+ * @notice creates a raffle
+ */
+contract Raffle {
+    uint256 private immutable iEntranceFee;
+
+    constructor(uint256 entranceFee) {
+        iEntranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < iEntranceFee) {
+            revert Raffle_NotEnoughETHEntered();
+        }
+        // TODO logic to enter the raffle
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return iEntranceFee;
+    }
+}
