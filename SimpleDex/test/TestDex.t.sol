29403ba17c4f985b9c409fa1a2a1b94f30dd65f2
SJ <sacha.jumpertz@gmail.com>
2026-03-28 16:17:47 +0100

    test deposit

--- SimpleDex/test/TestDex.t.sol
+++ SimpleDex/test/TestDex.t.sol
@@ -10,50 +10,112 @@ import {Balloons} from "../src/Balloons.sol";
 contract DEXTest is Test {
     DEX dex;
     Balloons balloons;
+    address INITIALIZER = makeAddr("initializer");
     address USER = makeAddr("user");
 
     function setUp() external {
         DeployBalloonsDEX deployer = new DeployBalloonsDEX();
         (dex, balloons) = deployer.run(address(this));
 
+        balloons.transfer(INITIALIZER, 100 ether);
         balloons.transfer(USER, 100 ether);
+
+        vm.startPrank(INITIALIZER);
+        balloons.approve(address(dex), 100 ether);
+        vm.stopPrank();
         vm.startPrank(USER);
         balloons.approve(address(dex), 100 ether);
         vm.stopPrank();
 
+        vm.deal(INITIALIZER, 10000 ether);
         vm.deal(USER, 10000 ether);
     }
 
     function testInit() external {
-        vm.startPrank(USER);
+        vm.startPrank(INITIALIZER);
         uint256 initialLiquidity = dex.init{value: 100 ether}(100 ether);
         vm.stopPrank();
 
         assertEq(initialLiquidity, 100 ether);
         assertEq(dex.totalLiquidity(), 100 ether);
-        assertEq(dex.getLiquidity(USER), 100 ether);
+        assertEq(dex.getLiquidity(INITIALIZER), 100 ether);
     }
 
     function testInitNotEnoughTokens() external {
-        vm.startPrank(USER);
+        vm.startPrank(INITIALIZER);
         vm.expectRevert();
         dex.init{value: 1000 ether}(1000 ether);
         vm.stopPrank();
     }
 
-    function testInitNotEnoughEth() external {
-        vm.startPrank(USER);
+    function testTokenAmountMismatch() external {
+        vm.startPrank(INITIALIZER);
         vm.expectRevert();
         dex.init{value: 0}(100 ether);
         vm.stopPrank();
     }
 
-    function testInitAlreadyInitialized() external {
-        vm.startPrank(USER);
+    function _initializeDex() internal {
+        vm.startPrank(INITIALIZER);
         dex.init{value: 100 ether}(100 ether);
+        vm.stopPrank();
+    }
 
+    function testInitAlreadyInitialized() external {
+        _initializeDex();
+
+        vm.startPrank(USER);
         vm.expectRevert();
         dex.init{value: 100 ether}(100 ether);
+        vm.stopPrank();
+    }
+
+    function testDeposit() external {
+        _initializeDex();
+        uint256 liquidityBeforeUser = dex.getLiquidity(USER);
+
+        vm.startPrank(USER);
+        uint256 tokenDeposit = dex.deposit{value: 10 ether}();
+        vm.stopPrank();
+
+        uint256 liquidityAfterUser = dex.getLiquidity(USER);
+        assertEq(liquidityAfterUser, liquidityBeforeUser + 10 ether);
+        assertEq(balloons.balanceOf(USER), 90 ether - 1); // -1 wei due to rounding in deposit
+        assertEq(tokenDeposit, 10 ether + 1); // deposit function adds +1 wei
+    }
+
+    function testDepositZeroEth() external {
+        _initializeDex();
+
+        vm.startPrank(USER);
+        vm.expectRevert();
+        dex.deposit{value: 0}();
+        vm.stopPrank();
+    }
+
+    function testDepositNotEnoughTokens() external {
+        _initializeDex();
+        address userWithNoTokens = makeAddr("userWithNoTokens");
+        vm.deal(userWithNoTokens, 10000 ether);
+        vm.startPrank(userWithNoTokens);
+        balloons.approve(address(dex), 100 ether);
+        vm.stopPrank();
+
+        vm.startPrank(userWithNoTokens);
+        vm.expectRevert();
+        dex.deposit{value: 1}();
+        vm.stopPrank();
+    }
+
+    function testDepositNotEnoughAllowance() external {
+        _initializeDex();
+        address userWithNoAllowance = makeAddr("userWithNoAllowance");
+        vm.deal(userWithNoAllowance, 10000 ether);
+        balloons.transfer(userWithNoAllowance, 100 ether);
+
+        vm.startPrank(userWithNoAllowance);
+        vm.expectRevert();
+        dex.deposit{value: 1}();
         vm.stopPrank();
     }
 }
