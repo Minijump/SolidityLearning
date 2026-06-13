e0a0de7c929edaf632242876ac84abf408e6d36c
SJ <sacha.jumpertz@gmail.com>
2026-06-13 15:27:37 +0200

    add scripts

--- SimpleNft/script/Interaction.s.sol
+++ SimpleNft/script/Interaction.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
-// import {MoodNft} from "../src/MoodNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG_URI =
@@ -22,30 +22,30 @@ contract MintBasicNft is Script {
     }
 }
 
-// contract MintMoodNft is Script {
-//     function run() external {
-//         address mostRecentlyDeployedMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
-//         mintNftOnContract(mostRecentlyDeployedMoodNft);
-//     }
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedMoodNft);
+    }
 
-//     function mintNftOnContract(address moodNftAddress) public {
-//         vm.startBroadcast();
-//         MoodNft(moodNftAddress).mintNft();
-//         vm.stopBroadcast();
-//     }
-// }
+    function mintNftOnContract(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
 
-// contract FlipMoodNft is Script {
-//     uint256 public constant TOKEN_ID_TO_FLIP = 0;
+contract FlipMoodNft is Script {
+    uint256 public constant TOKEN_ID_TO_FLIP = 0;
 
-//     function run() external {
-//         address mostRecentlyDeployedMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
-//         flipMoodNft(mostRecentlyDeployedMoodNft);
-//     }
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        flipMoodNft(mostRecentlyDeployedMoodNft);
+    }
 
-//     function flipMoodNft(address moodNftAddress) public {
-//         vm.startBroadcast();
-//         MoodNft(moodNftAddress).flipMood(TOKEN_ID_TO_FLIP);
-//         vm.stopBroadcast();
-//     }
+    function flipMoodNft(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNft(moodNftAddress).flipMood(TOKEN_ID_TO_FLIP);
+        vm.stopBroadcast();
+    }
 }
