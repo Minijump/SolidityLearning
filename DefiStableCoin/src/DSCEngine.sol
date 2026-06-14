f177287486ecae9fae83402c391a544c8c748efc
SJ <sacha.jumpertz@gmail.com>
2026-06-14 11:04:44 +0200

    add mint fct

--- DefiStableCoin/src/DSCEngine.sol
+++ DefiStableCoin/src/DSCEngine.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.19;
 
-// import { OracleLib, AggregatorV3Interface } from "./libraries/OracleLib.sol";
+import { OracleLib, AggregatorV3Interface } from "./libraries/OracleLib.sol"; // TODO
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { DecentralizedStableCoin } from "./DecentralizedStableCoin.sol";
@@ -19,27 +19,27 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__NeedsMoreThanZero();
     error DSCEngine__TokenNotAllowed(address token);
     error DSCEngine__TransferFailed();
-//     error DSCEngine__BreaksHealthFactor(uint256 healthFactorValue);
-//     error DSCEngine__MintFailed();
-//     error DSCEngine__HealthFactorOk();
-//     error DSCEngine__HealthFactorNotImproved();
+    error DSCEngine__BreaksHealthFactor(uint256 healthFactorValue);
+    error DSCEngine__MintFailed();
+    error DSCEngine__HealthFactorOk();
+    error DSCEngine__HealthFactorNotImproved();
 
-//     using OracleLib for AggregatorV3Interface;
+    using OracleLib for AggregatorV3Interface;
 
     DecentralizedStableCoin private immutable i_dsc;
 
 //     uint256 private constant LIQUIDATION_THRESHOLD = 50; // This means you need to be 200% over-collateralized
 //     uint256 private constant LIQUIDATION_BONUS = 10; // This means you get assets at a 10% discount when liquidating
 //     uint256 private constant LIQUIDATION_PRECISION = 100;
-//     uint256 private constant MIN_HEALTH_FACTOR = 1e18;
-//     uint256 private constant PRECISION = 1e18;
-//     uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
-//     uint256 private constant FEED_PRECISION = 1e8;
+    uint256 private constant MIN_HEALTH_FACTOR = 1e18;
+    uint256 private constant PRECISION = 1e18;
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant FEED_PRECISION = 1e8;
 
     mapping(address collateralToken => address priceFeed) private s_priceFeeds;
     mapping(address user => mapping(address collateralToken => uint256 amount)) private s_collateralDeposited;
-//     mapping(address user => uint256 amount) private s_DSCMinted;
-//     address[] private s_collateralTokens;
+    mapping(address user => uint256 amount) private s_DSCMinted;
+    address[] private s_collateralTokens;
 
     event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
 //     event CollateralRedeemed(address indexed redeemFrom, address indexed redeemTo, address token, uint256 amount); 
@@ -184,19 +184,19 @@ contract DSCEngine is ReentrancyGuard {
 //         _revertIfHealthFactorIsBroken(msg.sender);
 //     }
 
-//     /*
-//      * @param amountDscToMint: The amount of DSC you want to mint
-//      * You can only mint DSC if you have enough collateral
-//      */
-//     function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) nonReentrant {
-//         s_DSCMinted[msg.sender] += amountDscToMint;
-//         _revertIfHealthFactorIsBroken(msg.sender);
-//         bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+    /*
+     * @param amountDscToMint: The amount of DSC you want to mint
+     * You can only mint DSC if you have enough collateral
+     */
+    function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) nonReentrant {
+        s_DSCMinted[msg.sender] += amountDscToMint;
+        _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
 
-//         if (minted != true) {
-//             revert DSCEngine__MintFailed();
-//         }
-//     }
+        if (minted != true) {
+            revert DSCEngine__MintFailed();
+        }
+    }
 
     /*
      * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
@@ -246,49 +246,39 @@ contract DSCEngine is ReentrancyGuard {
 //         i_dsc.burn(amountDscToBurn);
 //     }
 
-//     function _getAccountInformation(address user)
-//         private
-//         view
-//         returns (uint256 totalDscMinted, uint256 collateralValueInUsd)
-//     {
-//         totalDscMinted = s_DSCMinted[user];
-//         collateralValueInUsd = getAccountCollateralValue(user);
-//     }
+    function _getAccountInformation(address user)
+        private
+        view
+        returns (uint256 totalDscMinted, uint256 collateralValueInUsd)
+    {
+        totalDscMinted = s_DSCMinted[user];
+        collateralValueInUsd = getAccountCollateralValue(user);
+    }
 
-//     function _healthFactor(address user) private view returns (uint256) {
-//         (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
-//         return _calculateHealthFactor(totalDscMinted, collateralValueInUsd);
-//     }
+    function _healthFactor(address user) private view returns (uint256) {
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+        return _calculateHealthFactor(totalDscMinted, collateralValueInUsd);
+    }
 
-//     function _getUsdValue(address token, uint256 amount) private view returns (uint256) {
-//         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
-//         (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
-//         // 1 ETH = 1000 USD
-//         // The returned value from Chainlink will be 1000 * 1e8
-//         // Most USD pairs have 8 decimals, so we will just pretend they all do
-//         // We want to have everything in terms of WEI, so we add 10 zeros at the end
-//         return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
-//     }
+    function _calculateHealthFactor(
+        uint256 totalDscMinted,
+        uint256 collateralValueInUsd
+    )
+        internal
+        pure
+        returns (uint256)
+    {
+        if (totalDscMinted == 0) return type(uint256).max;
+        uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
+        return (collateralAdjustedForThreshold * PRECISION) / totalDscMinted;
+    }
 
-//     function _calculateHealthFactor(
-//         uint256 totalDscMinted,
-//         uint256 collateralValueInUsd
-//     )
-//         internal
-//         pure
-//         returns (uint256)
-//     {
-//         if (totalDscMinted == 0) return type(uint256).max;
-//         uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
-//         return (collateralAdjustedForThreshold * PRECISION) / totalDscMinted;
-//     }
-
-//     function _revertIfHealthFactorIsBroken(address user) internal view {
-//         uint256 userHealthFactor = _healthFactor(user);
-//         if (userHealthFactor < MIN_HEALTH_FACTOR) {
-//             revert DSCEngine__BreaksHealthFactor(userHealthFactor);
-//         }
-//     }
+    function _revertIfHealthFactorIsBroken(address user) internal view {
+        uint256 userHealthFactor = _healthFactor(user);
+        if (userHealthFactor < MIN_HEALTH_FACTOR) {
+            revert DSCEngine__BreaksHealthFactor(userHealthFactor);
+        }
+    }
 
 //     ////////////////////////////////////////////////////////////////////////////
 //     ////////////////////////////////////////////////////////////////////////////
@@ -314,6 +304,16 @@ contract DSCEngine is ReentrancyGuard {
 //         return _getAccountInformation(user);
 //     }
 
+    function _getUsdValue(address token, uint256 amount) private view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
+        // 1 ETH = 1000 USD
+        // The returned value from Chainlink will be 1000 * 1e8
+        // Most USD pairs have 8 decimals, so we will just pretend they all do
+        // We want to have everything in terms of WEI, so we add 10 zeros at the end
+        return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
+    }
+
 //     function getUsdValue(
 //         address token,
 //         uint256 amount // in WEI
@@ -329,14 +329,14 @@ contract DSCEngine is ReentrancyGuard {
 //         return s_collateralDeposited[user][token];
 //     }
 
-//     function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
-//         for (uint256 index = 0; index < s_collateralTokens.length; index++) {
-//             address token = s_collateralTokens[index];
-//             uint256 amount = s_collateralDeposited[user][token];
-//             totalCollateralValueInUsd += _getUsdValue(token, amount);
-//         }
-//         return totalCollateralValueInUsd;
-//     }
+    function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
+        for (uint256 index = 0; index < s_collateralTokens.length; index++) {
+            address token = s_collateralTokens[index];
+            uint256 amount = s_collateralDeposited[user][token];
+            totalCollateralValueInUsd += _getUsdValue(token, amount);
+        }
+        return totalCollateralValueInUsd;
+    }
 
 //     function getTokenAmountFromUsd(address token, uint256 usdAmountInWei) public view returns (uint256) {
 //         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
