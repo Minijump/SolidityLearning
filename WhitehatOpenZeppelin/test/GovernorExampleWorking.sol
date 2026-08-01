768003f088d2bc13016e32b0e42b2ef295ebc934
SJ <sacha.jumpertz@gmail.com>
2026-08-01 13:35:00 +0200

    add new test to test propose

--- WhitehatOpenZeppelin/test/GovernorExampleWorking.sol
+++ WhitehatOpenZeppelin/test/GovernorExampleWorking.sol
@@ -29,22 +29,20 @@ contract GovernorExampleTest is Test {
         assertEq(governor.getVotes(address(0), 0), 10e18);
     }
 
-    function testSetVotingDelayOnlyThroughGovernanceProposal() public {
+    function testSetVotingDelayDirectCallFails() public {
         uint48 newVotingDelay = 2;
-
-        // Direct call should fail because setVotingDelay is onlyGovernance.
         vm.expectRevert();
         governor.setVotingDelay(newVotingDelay);
+    }
 
+    function testSetVotingDelayOnlyThroughGovernanceProposal() public {
+        uint48 newVotingDelay = 2;
         address[] memory targets = new address[](1);
         targets[0] = address(governor);
-
         uint256[] memory values = new uint256[](1);
         values[0] = 0;
-
         bytes[] memory calldatas = new bytes[](1);
         calldatas[0] = abi.encodeCall(GovernorSettings.setVotingDelay, (newVotingDelay));
-
         string memory description = "Set voting delay to 2 blocks";
         bytes32 descriptionHash = keccak256(bytes(description));
 
@@ -53,11 +51,25 @@ contract GovernorExampleTest is Test {
         // Move past snapshot so voting is active.
         vm.roll(block.number + governor.votingDelay() + 1);
         governor.castVote(proposalId, 1); // 1 = For
-
         // Move past deadline so proposal can be executed.
         vm.roll(block.number + governor.votingPeriod() + 1);
         governor.execute(targets, values, calldatas, descriptionHash);
-
         assertEq(governor.votingDelay(), newVotingDelay);
+    }
+
+    function testProposeMisc() public {
+        address[] memory targets = new address[](1);
+        targets[0] = address(0);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = "";
+        string memory description = "Misc proposal";
+
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        // check porposal state is active after voting delay
+        vm.roll(block.number + governor.votingDelay() + 1);
+        assertEq(uint8(governor.state(proposalId)), uint8(IGovernor.ProposalState.Active));
     }
 }
