2f7a7edb1018029ae9f42a4343aedce3ed67b53e
SJ <sacha.jumpertz@gmail.com>
2026-08-01 13:50:30 +0200

    add vote unit test

--- WhitehatOpenZeppelin/test/GovernorExampleWorking.sol
+++ WhitehatOpenZeppelin/test/GovernorExampleWorking.sol
@@ -72,4 +72,71 @@ contract GovernorExampleTest is Test {
         vm.roll(block.number + governor.votingDelay() + 1);
         assertEq(uint8(governor.state(proposalId)), uint8(IGovernor.ProposalState.Active));
     }
+
+    function testVoteProposal() public {
+        uint48 newVotingDelay = 2;
+        address[] memory targets = new address[](1);
+        targets[0] = address(governor);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeCall(GovernorSettings.setVotingDelay, (newVotingDelay));
+        string memory description = "Set voting delay to 2 blocks";
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        // Move past snapshot so voting is active.
+        vm.roll(block.number + governor.votingDelay() + 1);
+        governor.castVote(proposalId, 1); // 1 = For
+
+        // count votes
+        assertEq(governor.hasVoted(proposalId, address(this)), true);
+        (uint256 againstVotes, uint256 forVotes, uint256 abstainVotes) = governor.proposalVotes(proposalId);
+        assertEq(againstVotes, 0);
+        assertEq(forVotes, 10e18);
+        assertEq(abstainVotes, 0);
+    }
+
+    function testAcceptedProposalIsExecuted() public {
+        uint48 newVotingDelay = 2;
+        address[] memory targets = new address[](1);
+        targets[0] = address(governor);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeCall(GovernorSettings.setVotingDelay, (newVotingDelay));
+        string memory description = "Set voting delay to 2 blocks";
+        bytes32 descriptionHash = keccak256(bytes(description));
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        // Move past snapshot so voting is active.
+        vm.roll(block.number + governor.votingDelay() + 1);
+        governor.castVote(proposalId, 1); // 1 = For
+
+        // Move past deadline so proposal can be executed.
+        vm.roll(block.number + governor.votingPeriod() + 1);
+        governor.execute(targets, values, calldatas, descriptionHash);
+        assertEq(governor.votingDelay(), newVotingDelay);
+    }
+
+    function testRefusedProposalIsNotExecuted() public {
+        uint48 newVotingDelay = 2;
+        address[] memory targets = new address[](1);
+        targets[0] = address(governor);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeCall(GovernorSettings.setVotingDelay, (newVotingDelay));
+        string memory description = "Set voting delay to 2 blocks";
+        bytes32 descriptionHash = keccak256(bytes(description));
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        // Move past snapshot so voting is active.
+        vm.roll(block.number + governor.votingDelay() + 1);
+        governor.castVote(proposalId, 0); // 0 = Against
+
+        // Move past deadline so proposal can be executed.
+        vm.roll(block.number + governor.votingPeriod() + 1);
+        vm.expectRevert();
+        governor.execute(targets, values, calldatas, descriptionHash);
+    }
 }
